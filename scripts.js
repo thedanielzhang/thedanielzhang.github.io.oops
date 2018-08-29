87e33e5b7e9386809ce02f9e26aa85b22030dd17
Daniel <daniel.zhang@berkeley.edu>
2018-08-29 16:10:59 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    $("#landing").click(function() {
+        // $("#content")
+        $("#content").css('background-color', 'blue');
+    });
+
+    $("#about").click(function() {
+        // $("#content")
+        $("#content").css('background-color', 'red');
+    });
+
+    $("#projects").click(function() {
+        // $("#content")
+        $("#content").css('background-color', 'green');
+    });
+
+    $("#resume").click(function() {
+        // $("#content")
+        $("#content").css('background-color', 'yellow');
+    });
+
+    $("#experience").click(function() {
+        // $("#content")
+        $("#content").css('background-color', 'orange');
+    });
+
+    $("#contact").click(function() {
+        // $("#content")
+        $("#content").css('background-color', 'purple');
+    });
+ 
+ });
